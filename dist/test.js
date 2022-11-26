1577e05e0c60de2ed41fd5196a89c84bbf1ed77c
inka <inka_salminen@hotmail.com>
2022-11-26 13:30:12 +0200

    there will be a branch

--- dist/test.js
+++ dist/test.js
@@ -1,8 +1,30 @@
 "use strict";
 const express = require('express');
 const app = express();
-app.get('/', function (req, res) {
-    res.send('Hello Git');
+const { expressjwt: jwt } = require('express-jwt');
+const { readFileSync } = require('fs');
+const debug = require('debug')('app');
+const fileName = './public-key.pem';
+const contents = readFileSync(fileName, 'utf-8');
+let tester = 'Hello JWT';
+if (tester !== 'Hello Token!') {
+    tester = 'Hello Json Web Token';
+}
+else {
+    tester = 'Goodbye Token';
+}
+app.use(jwt({ secret: contents, algorithms: ["RS256"] }));
+app.get('/protected', (req, res) => {
+    res.send('Hello JWT');
 });
-console.log('Other branch annihilated');
-app.listen(6666);
+// Set port from env or by default 8080
+const port = +(process.env.PORT || '8080');
+const server = app.listen(port, () => {
+    debug(`Running on ${port}`);
+});
+process.on('SIGTERM', () => {
+    debug('SIGTERM signal received, closing the HTTP server');
+    server.close(() => {
+        debug('HTTP server closed');
+    });
+});
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { expressjwt: jwt } = require('express-jwt');
+const { readFileSync } = require('fs');
+const debug = require('debug')('app');
 
-app.get('/', function (req: any, res: { send: (arg0: string) => void; }) {
-  res.send('Hello Git')
-})
-console.log('Other branch annihilated');
+const fileName: string = './public-key.pem';
+const contents: string = readFileSync(fileName, 'utf-8');
 
-app.listen(6666);
+let tester: string = 'Hello JWT';
+if (tester !== 'Hello Token!') {
+  tester = 'Hello Json Web Token';
+} else {
+  tester = 'Goodbye Token';
+}
+app.use(jwt({ secret: contents, algorithms: ["RS256"] }));
+
+app.get('/protected', (req: any, res: { send: (arg0: string) => void; }) => {
+  res.send('Hello JWT');
+});
+
+// Set port from env or by default 8080
+const port: number = +(process.env.PORT || '8080');
+const server = app.listen(port, () => {
+  debug(`Running on ${port}`);
+});
+  
+process.on('SIGTERM', () => {
+  debug('SIGTERM signal received, closing the HTTP server');
+  server.close(() => {
+    debug('HTTP server closed');
+  });
+});
+
